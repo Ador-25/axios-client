305296d104af5e2456aa9267ca0701c80ddaaa7f
Abtahee Farhan Ador <abtahee.ador25@gmail.com>
2023-02-11 12:56:03 +0600

    auth tested

--- Auth.js
+++ Auth.js
@@ -1,5 +1,3 @@
-const { default: axios } = require("axios")
-
 const prefix ='https://localhost:7209/api/Authenticate/'
 
 //gets parameter username & password returns a token if successful
@@ -41,19 +39,38 @@ async function registerSchool(
     SchoolDescription,
     SchoolTypeOfInstitution,
     SchoolCurriculumType){
-        const endpoint = prefix+'register-school'
-        const res= await axios.post(endpoint,
-            {
-                email:SchoolEmail,
-                password:SchoolPassword,
-                schoolName:SchoolName,
-                schoolCode: SchoolCode,
-                phoneNumber: SchoolPhoneNumber,
-                address:SchoolAddress,
-                SchoolDescription: SchoolDescription,
-                typeOfInstitution:SchoolTypeOfInstitution,
-                curriculumType:SchoolCurriculumType
-            }
-        )
+        //console.log(prefix+'register-school')
+        const mail=SchoolEmail
+        const pass=SchoolPassword
+        const name=SchoolName
+        const code=SchoolCode
+        const phn=SchoolPhoneNumber
+        const addr= SchoolAddress
+        const dsc= SchoolDescription
+        const t_ins= SchoolTypeOfInstitution
+        const t_curr=SchoolCurriculumType
+
+        try{
+            const res= await axios.post('https://localhost:7209/api/Authenticate/register-school',
+                {
+                    email:mail,
+                    password:pass,
+                    schoolName:name,
+                    schoolCode: code,
+                    phoneNumber: phn,
+                    address:addr,
+                    SchoolDescription: dsc,
+                    typeOfInstitution:t_ins,
+                    curriculumType:t_curr
+                }
+            )
+            console.log('res:')
+            console.log(res.data)
+            return res
+        }
+        catch(err){
+            console.log(err)
+        }
+
 
 }
